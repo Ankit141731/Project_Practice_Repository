6417369261dcee53699dd6a126977671607e567f
Ankit141731 <ac7265036@gmail.com>
2024-11-23 17:40:16 +0530

    setup and requirements file

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-1
+from setuptools import setup , find_packages
+
+setup(
+
+    name = "experiment" , 
+    version = "0.0.0" , 
+    author = "Ankit" , 
+    author_email = "ac7265036@gmail.com" ,
+    packages = find_packages()
+
+)
